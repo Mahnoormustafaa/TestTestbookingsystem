9954b8a5ab572378cfb05a6a90026dbf783da623
Mahnoormustafaa <mahnoormustafa318@gmail.com>
2024-11-24 07:41:20 -0800

    Add files via upload

---
+++ Testbookingsystem.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package testbookingsystem;
+
+/**
+ *
+ * @author hp
+ */
+public class Testbookingsystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     loginn login=new loginn();
+     login.setLocationRelativeTo(null);
+     login.setVisible(true);
+     
+     
+    }
+    
+}
